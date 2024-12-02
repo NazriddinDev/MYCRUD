c76b79af5abbcd06a7be65f020192d12c3c2a798
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-12-02 12:05:06 +0500

    Add Context class with Technique Controller

---
+++ MYCRUD/Controllers/TechniquesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MYCRUD.Controllers
+{
+    public class TechniquesController : Controller
+    {
+        public IActionResult AddTechnique()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MYCRUD/Data/MVCDemoDbContext.cs
+++ MYCRUD/Data/MVCDemoDbContext.cs
@@ -11,5 +11,6 @@ namespace MYCRUD.Data
 
 
         public DbSet<Employee> Employees{ get; set; }
+        public DbSet<Technique> Techniques{ get; set; }
     }
 }
