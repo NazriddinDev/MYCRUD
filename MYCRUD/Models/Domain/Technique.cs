455fd502cfed7590a7ec721276a5b656871e905f
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-12-02 11:59:44 +0500

    Add Technique model

---
+++ MYCRUD/Models/Domain/Technique.cs
@@ -0,0 +1,10 @@
+﻿namespace MYCRUD.Models.Domain
+{
+    public class Technique
+    {
+        public Guid Id { get; set; }
+        public string TechName { get; set; }
+        public string TechModel { get; set; }
+        public string Heavy { get; set; }
+    }
+}
